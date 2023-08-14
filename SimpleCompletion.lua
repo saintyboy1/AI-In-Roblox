b575152f9aa9dcfaffe472bc00c2d0ffd8fe0127
saintyboy1 <59389065+saintyboy1@users.noreply.github.com>
2023-08-14 10:59:55 +0100

    Update SimpleCompletion.lua

--- SimpleCompletion.lua
+++ SimpleCompletion.lua
@@ -2,12 +2,12 @@
 -- Author: saintyboy1
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Model = require(ReplicatedStorage["TEXT-SNT"])
+local Model = require(ReplicatedStorage["MODEL_TYPE_HERE"])
 
 
-local Output = Model:NewCompletion() -- It's as simple as this !
+local Output = Model:NewCompletion() -- It's as simple as this ! Note that this is for a simple completion with the model's defaults. It takes input as the parameters in this usage.
 
---[[ If you'd like to create a text completion with hyperparameters then
+--[[ If you'd like to create a proper text completion with parameters then
 	please look below for an example. You can adjust the parameters to how you'd like.
 	
 	
