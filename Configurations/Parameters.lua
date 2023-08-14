abc642fe9bbd066a991fd6859fa75dfde881e9a3
saintyboy1 <59389065+saintyboy1@users.noreply.github.com>
2023-08-14 11:39:31 +0100

    Create Parameters.lua

---
+++ Configurations/Parameters.lua
@@ -0,0 +1,10 @@
+-- PARAMETERS.LUA
+-- Author: saintyboy1
+
+local parameters = {
+	KEY = "YOUR_API_KEY"; -- You will insert your API key here which you can generate in our discord fpr free
+	INPUT = "YOUR_INPUT"; -- Self explanatory: the prompt for the model to use
+	ENDPOINT = "https://api.s41nt.xyz"; -- This is the API endpoint. I only advise you change this if you know what you're doing! If you want to run it on your machine then look at the Backend part in this repo.
+}
+
+return parameters
