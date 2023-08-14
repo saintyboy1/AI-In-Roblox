5582d78110c5627b5f3d32b318406369e0052f6e
saintyboy1 <59389065+saintyboy1@users.noreply.github.com>
2023-08-14 11:35:47 +0100

    Create Fail.lua

---
+++ Configurations/Fail.lua
@@ -0,0 +1,13 @@
+-- PARAMETERS.LUA
+-- Author: saintyboy1
+
+local parameters = {
+	KEY = "5369dcb52e4fc6feb8bcdff3f09a342d";
+	INPUT = "Quick test!";
+	ENDPOINT = "https://0b46-81-153-138-79.ngrok-free.app"; -- DON'T change this unless you know what you're doing
+}
+
+return parameters
+
+
+-- PLEASE put these configs in a folder called "Configurations" inside of the root module you are using.
