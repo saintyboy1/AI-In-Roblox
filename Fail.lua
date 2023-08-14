7d17b55f6b89cf1c40c9ac3cafaa2d89ccd75357
saintyboy1 <59389065+saintyboy1@users.noreply.github.com>
2023-08-14 11:35:10 +0100

    Create Fail.lua

---
+++ Fail.lua
@@ -0,0 +1,10 @@
+-- FAIL.LUA
+-- Author: saintyboy1
+-- PLEASE put these configs in a folder called "Configurations" inside of the root module you are using.
+
+local fail = {
+	MAX_RETRIES = 5;
+	ATTEMPTS = 0;
+}
+
+return fail
